6fcb21914d75a1811404d5eaa5aa76a4de469d2d
vuquangphong <thapquanviet@gmail.com>
2021-11-13 15:08:23 +0700

    search module backend

--- server/src/controllers/controllers.song.search.js
+++ server/src/controllers/controllers.song.search.js
@@ -9,10 +9,13 @@ controller.searchControllers = async (req, res) => {
     if (!titleSearch) {
         return res.status(400).json({ success: false, message: 'please enter something -_-' });
     }
-    console.log(titleSearch);
 
     try {
-        const songs = await Song.find({ title: titleSearch }).populate('user', ['fullname']);
+        const searchCondition = new RegExp(`${titleSearch}`, 'i');
+
+        const songs = await Song
+            .find({ title: searchCondition }, { _id: 0, __v: 0 })
+            .populate('user', 'fullname -_id');
 
         if (songs.length === 0) {
             return res.status(404).json({ success: false, message: 'Sorry, something you want is not available :((' });
